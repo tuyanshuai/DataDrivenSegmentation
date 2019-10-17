d6809855364a8db463ba2b023c344b47d11f1039
antoniechan <51134610+antoniechan@users.noreply.github.com>
2019-10-17 17:01:45 +0800

    Add files via upload

--- SimpleDemon.py
+++ SimpleDemon.py
@@ -3,9 +3,9 @@
 import numpy as np
 import scipy.ndimage
 from skimage.transform import PiecewiseAffineTransform, warp
-from skimage import data
 import matplotlib.pyplot as plt
 from PIL import Image
+from scipy.ndimage import map_coordinates
 
 
 def gaussian_filter(dim1, dim2):
@@ -21,47 +21,24 @@ def gaussian_filter(dim1, dim2):
     return h
 
 
-def simple_demon(im_moving, im_static, times, gaussian_size):
+def simple_demon(im_moving, im_static, times, gaussian_size, alpha):
     s = np.array(im_static.convert('L')) / 255
     m = np.array(im_moving.convert('L')) / 255
-    rows, cols = s.shape[0], s.shape[1]
-    src_cols = np.linspace(0, cols, cols)
-    src_rows = np.linspace(0, rows, rows)
-    src_rows, src_cols = np.meshgrid(src_rows, src_cols)
-    src = np.dstack([src_cols.flat, src_rows.flat])[0]
-    alpha = 2.5
+    field = np.meshgrid(np.linspace(0, s.shape[0], s.shape[0]), np.linspace(0, s.shape[1], s.shape[1]))
     h_smooth = gaussian_filter(gaussian_size[0:2], gaussian_size[2])
-    t_rows = src_rows
-    t_cols = src_cols
     dsdy, dsdx = np.gradient(s)
     for ii in range(times):
         image_diff = m - s
         dmdy, dmdx = np.gradient(m)
-        d1 = (dsdy ** 2 + dsdx ** 2) + (alpha ** 2) * (image_diff ** 2)
-        d2 = (dmdy ** 2 + dmdx ** 2) + (alpha ** 2) * (image_diff ** 2)
-        temp1 = np.array(np.where(d1 != 0))
-        temp2 = np.array(np.where(d2 != 0))
-        d1[temp1[0], temp1[1]] = 1 / d1[temp1[0], temp1[1]]
-        d2[temp2[0], temp2[1]] = 1 / d2[temp2[0], temp2[1]]
-        u_cols = -image_diff * (dsdy * d1 + dmdy * d2)
-        u_rows = -image_diff * (dsdx * d1 + dmdx * d2)
+        u_cols = -image_diff * (dsdy * ((dsdy ** 2 + dsdx ** 2) + (alpha ** 2) * (image_diff ** 2)) + dmdy * (
+                    (dmdy ** 2 + dmdx ** 2) + (alpha ** 2) * (image_diff ** 2)))
+        u_rows = -image_diff * (dsdx * ((dsdy ** 2 + dsdx ** 2) + (alpha ** 2) * (image_diff ** 2)) + dmdx * (
+                    (dmdy ** 2 + dmdx ** 2) + (alpha ** 2) * (image_diff ** 2)))
+        u_cols[np.isnan(u_cols)] = 0
+        u_rows[np.isnan(u_rows)] = 0
         u_cols = 3 * scipy.ndimage.correlate(u_cols, h_smooth, mode='constant')
         u_rows = 3 * scipy.ndimage.correlate(u_rows, h_smooth, mode='constant')
-        t_rows += 100 * u_rows
-        t_cols += 100 * u_cols
-        dst = np.dstack([t_cols.flat, t_rows.flat])[0]
-        tform = PiecewiseAffineTransform()
-        tform.estimate(src, dst)
-        out = warp(im_moving, tform)[:, :, 0:3]
-        out[:, :, 0] = out[:, :, 0].T
-        out[:, :, 1] = out[:, :, 1].T
-        out[:, :, 2] = out[:, :, 2].T
-        m = np.array(Image.fromarray(np.uint8(out)).convert('L'))
-        fig, ax = plt.subplots()
-        ax.imshow(m - s)
-        ax.axis((0, rows, cols, 0))
-        plt.show()
-    fig, ax = plt.subplots()
-    ax.imshow(out)
-    ax.axis((0, rows, cols, 0))
-    plt.show()
+        field[0] += u_rows
+        field[1] += u_cols
+        m = map_coordinates(m, field, mode='nearest')
+    return field
